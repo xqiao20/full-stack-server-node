f3174ae4c4c02383e5955df8b611d1272f9833ea
xqiao20 <xqiao18@gmail.com>
2022-04-03 23:47:59 -0400

    Update port

--- server.js
+++ server.js
@@ -12,4 +12,4 @@ userController(app);
 tuitsController(app);
 // app.get('/hello', (req, res) => {res.send('Hello World!')})
 app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
-app.listen(4000);
+app.listen(process.env.PORT ||4000);
