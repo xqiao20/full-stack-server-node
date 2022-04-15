6863f2d25a97ab91ca7abae2680583b6f1ad23d1
xqiao20 <xqiao18@gmail.com>
2022-04-15 13:07:49 -0400

    Link to atlas

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ import helloController from "./controllers/hello-controller.js";
 import userController from "./controllers/user-controller.js";
 import tuitsController from "./controllers/tuits-controller.js";
 
-mongoose.connect('mongodb://localhost:27017/webdev')
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://localhost:27017/webdev'
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors());
 app.use(express.json());
